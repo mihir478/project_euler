f1ac49266c4e2d8f8f114627acf5577d6f8b86cd
Mihir478 <mihir.h.sanghavi@gmail.com>
2019-01-13 10:46:39 -0500

    P2

---
+++ P2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace P2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            for (int a0 = 0; a0 < t; a0++)
+            {
+                long n = Convert.ToInt64(Console.ReadLine());
+                Console.WriteLine(SumEven(n));
+            }
+            Console.Read();
+        }
+
+        static long SumEven(long n)
+        {
+            long a = 0, b = 2, sum = 0;
+            while (b < n)
+            {
+                sum += b;
+                b = a + 4 * b;
+                a = (b - a) / 4;
+            }
+            return sum;
+        }
+    }
+}
