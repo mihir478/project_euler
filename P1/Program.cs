afdfadbe49eb9446248d9700c3a17a610b38a591
Mihir478 <mihir.h.sanghavi@gmail.com>
2019-01-13 10:40:08 -0500

    P1

---
+++ P1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace P1
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            for (int a0 = 0; a0 < t; a0++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(Calc(n));
+            }
+            Console.ReadKey();
+        }
+
+        private static ulong Calc(int N)
+        {
+            // multiples less than N
+            ulong mult3 = (ulong)(N - 1) / 3,
+                mult5 = (ulong)(N - 1) / 5,
+                mult15 = (ulong)(N - 1) / 15;
+
+            ulong sum3 = SumNatural(mult3),
+                sum5 = SumNatural(mult5),
+                sum15 = SumNatural(mult15);
+
+            return 3 * sum3 + 5 * sum5 - 15 * sum15;
+        }
+
+        private static ulong SumNatural(ulong n)
+        {
+            return n * (n + 1) / 2;
+        }
+    }
+}
