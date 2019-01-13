3660ffb3f1cd8c1831db4fa439ecc17698975ece
Mihir478 <mihir.h.sanghavi@gmail.com>
2019-01-13 13:51:56 -0500

    P4

--- P4/Program.cs
+++ P4/Program.cs
@@ -6,7 +6,30 @@ namespace P4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int t = Convert.ToInt32(Console.ReadLine());
+            for (int a0 = 0; a0 < t; a0++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(largestPalindrome(n));
+            }
+            Console.ReadKey();
+        }
+
+        static int largestPalindrome(int n)
+        {
+            int maxPalin = 0;
+            for (int i = 999; i > 100; --i)
+                for (int j = n / i; i * j > maxPalin && j > 100; --j)
+                {
+                    int product = i * j;
+                    if (product % 11 != 0) continue;
+                    string s = product + "";
+                    char[] chars = s.ToCharArray();
+                    Array.Reverse(chars);
+                    if (s == new string(chars) && product > maxPalin && product < n)
+                        maxPalin = product;
+                }
+            return maxPalin;
         }
     }
 }
