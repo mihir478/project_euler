bfd4009cf907124d13c72546c8b17adab2598c1e
Mihir478 <mihir.h.sanghavi@gmail.com>
2019-08-27 07:55:28 -0400

    P16

---
+++ P16/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Numerics;
+
+namespace P16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            for (int a0 = 0; a0 < t; ++a0)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+                BigInteger b = BigInteger.Pow(2, n);
+                string digits = b.ToString();
+                int sum = 0;
+                for (int i = 0; i < digits.Length; ++i)
+                {
+                    sum += Convert.ToInt32(digits[i] - '0');
+                }
+                Console.WriteLine(sum);
+                Console.ReadKey();
+            }
+        }
+    }
+}
